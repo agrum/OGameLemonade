8e09c6c599389996c15ceb698b48b473f07787d9
Agrum <thomas.lgd@gmail.com>
2016-06-25 11:45:10 -0700

    add back language files and parser

---
+++ include/en/lang.php
@@ -0,0 +1,36 @@
+<?php
+
+include '../include/short.php';
+
+//Full name pokedex
+$name = array(
+	$ligFi => 'Light Fighter',
+	$heaFi => 'Heavy Fighter',
+	$cruis => 'Cruiseur',
+	$batSh => 'Battleship',
+	$bombe => 'Bomber',
+	$destr => 'Destructor',
+	$death => 'RIP',
+	$batCr => 'Battlecruiser',
+
+	$smCar => 'Small Cargo',
+	$laCar => 'Large Cargo',
+	$recyc => 'Recyclor',
+	$colon => 'Colonization Ship',
+	$probe => 'Probe',
+	$solar => 'Solar',
+
+	$roLau => 'Rocket Launcher',
+	$liLas => 'Light Laser',
+	$heLas => 'Heavy Laser',
+	$ionCa => 'Ion Canon',
+	$gauss => 'Gauss Canon',
+	$plasm => 'Plasma Turret',
+	$smShi => 'Small Shield',
+	$laShi => 'Large Shield',
+
+	$misIP => 'MIP',
+	$misIn => 'MInt',
+);
+
+?>
